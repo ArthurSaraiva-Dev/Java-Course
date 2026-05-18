f33784c1bfcb4a4e24832a7c4c42566ab641f9fa
Arthur Saraiva <arthursaraiva.dev@gmail.com>
2026-05-18 18:35:38 -0300

    exemplo de java doc

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,5 +1,10 @@
 package fundamentos;
 
+/**
+ * Meu Hello World
+ * @author Arthur Saraiva
+ */
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         System.out.println("Hello World"); // Imprime na saída "Hello World"
