8224566a86fe74cce6026efb659df2d03c94a297
Arthur Saraiva <arthursaraiva.dev@gmail.com>
2026-05-20 13:04:03 -0300

    Inferência de tipos

---
+++ exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class Inferencia {
+    public static void main(String[] args) {
+        double a = 6.9; // declaração e inicialização
+        System.out.println(a); // uso da variável
+
+        var b = 4.5; // "var" é uma inferência de tipos, que detecta automaticamente o tipo do dado.
+        var c = "frase"; // string
+
+    }
+}
