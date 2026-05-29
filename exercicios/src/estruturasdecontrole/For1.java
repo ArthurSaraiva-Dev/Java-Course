7239571e2655b20185ebdcecb2a51c7df060bf01
Arthur Saraiva <arthursaraiva.dev@gmail.com>
2026-05-29 15:33:58 -0300

    feat: For
    Trabalhando com a estrutura de controle For

---
+++ exercicios/src/estruturasdecontrole/For1.java
@@ -0,0 +1,11 @@
+package estruturasdecontrole;
+
+public class For1 {
+    public static void main(String[] args) {
+        for (int contador = 1; contador <= 10; contador++) { // Focado em quantidades determinadas
+            System.out.printf("Contador: %d\n", contador);
+        }
+
+
+    }
+}
