2e19d94a14bf4a48ee22889b6f8f487603f737e5
Arthur Saraiva <arthursaraiva.dev@gmail.com>
2026-06-04 00:09:11 -0300

    feat: Desafio: Refatorando com Método

--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -12,8 +12,11 @@ public class DataTeste {
         d2.mes = 10;
         d2.ano = 2005;
 
-        System.out.printf("%d/%d/%d\n", d1.dia,d1.mes,d1.ano);
-        System.out.printf("%d/%d/%d\n", d2.dia,d2.mes,d2.ano);
+        //System.out.printf("%d/%d/%d\n", d1.dia,d1.mes,d1.ano);
+        //System.out.printf("%d/%d/%d\n", d2.dia,d2.mes,d2.ano);
+
+        System.out.println(d1.obterDataFromatada());
+        System.out.println(d2.obterDataFromatada());
 
     }
 
