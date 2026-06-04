5e68fc55dec83e40955db697cb3ae72433ecb510
Arthur Saraiva <arthursaraiva.dev@gmail.com>
2026-06-04 08:19:17 -0300

    feat: Exercitando Constructor

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -5,6 +5,20 @@ public class Data {
     int mes;
     int ano;
 
+
+
+    Data(){
+        dia = 1;
+        mes = 1;
+        ano = 1970;
+    }
+
+    Data(int diaInicial, int mesInicial, int anoIncial){
+        dia = diaInicial;
+        mes = mesInicial;
+        ano = anoIncial;
+    }
+
     String obterDataFromatada(){
         return String.format("%d/%d/%d", dia, mes, ano);
     }
--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -2,15 +2,15 @@ package classe;
 
 public class DataTeste {
     public static void main(String[] args) {
-        Data d1 = new Data();
-        d1.dia = 22;
-        d1.mes = 7;
-        d1.ano = 2004;
+        Data d1 = new Data(); // DATA DEFINICA PADRÃO 1/1/1970
+        //d1.dia = 1;
+        //d1.mes = 1;
+        //d1.ano = 1970;
 
-        Data d2 = new Data();
-        d2.dia = 29;
-        d2.mes = 10;
-        d2.ano = 2005;
+        Data d2 = new Data(22, 7,2004); // Utilizando Constructor
+        //d2.dia = 22;
+        //d2.mes = 7;
+        //d2.ano = 2004;
 
         //System.out.printf("%d/%d/%d\n", d1.dia,d1.mes,d1.ano);
         //System.out.printf("%d/%d/%d\n", d2.dia,d2.mes,d2.ano);
