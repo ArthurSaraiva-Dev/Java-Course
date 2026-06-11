aed57748c2e54c489c33a560df3a4ec0d0de5631
Arthur Saraiva <arthursaraiva.dev@gmail.com>
2026-06-10 22:03:52 -0300

    add: Desafio

---
+++ exercicios/src/classe/Desafio.java
@@ -0,0 +1,14 @@
+package classe;
+
+import fundamentos.PrimeiroPrograma;
+
+public class Desafio {
+    int a = 5;
+    static int b = 4;
+
+    public static void main(String[] args) {
+        Desafio p = new Desafio();
+        System.out.println(p.a);
+        System.out.println(b);
+    }
+}
